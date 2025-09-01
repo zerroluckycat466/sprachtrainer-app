4a2d63515885a27c0e973f988f8c14862e7bef25
zerroluckycat466 <m98jtlgb0nbs@hotmail.com>
2025-09-01 23:38:37 +0800

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Willkommen zur Sprachtrainer App</h1>
+    </div>
+  );
+}
+
+export default App;
