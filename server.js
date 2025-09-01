76eaae9af750c37654942947a9e0d6c34db748e8
zerroluckycat466 <m98jtlgb0nbs@hotmail.com>
2025-09-01 23:38:36 +0800

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect('mongodb://localhost/sprachtrainer', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Willkommen zur Sprachtrainer App API!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server läuft auf Port: ${PORT}`);
+});
